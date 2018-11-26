0716001f6133f627484f6f96de530acd9068e563
meng <dz6168@163.com>
2018-11-26 13:57:35 +0800

    gg

---
+++ AIWenzhang/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWenzhang
+{
+    class Article
+    {
+        String title;
+        String keywords;
+        String description;
+        String kk;
+
+
+    }
+}
---
+++ AIWenzhang/Title.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWenzhang
+{
+    class Title
+    {
+        int id;
+        String name;
+        String gettitle()
+        {
+
+            return name;
+        }
+
+        public void settitle(String title)
+        {
+            this.name = title;
+        }
+
+    }
+}
