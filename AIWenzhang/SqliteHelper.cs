60eef7c99baf389c386c656070ec054e77701a44
meng <dz6168@163.com>
2018-12-17 20:22:51 +0800

    得到

---
+++ AIWenzhang/SqliteHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class SqliteHelper
+{
+	public SqliteHelper()
+	{
+        SQLiteConnection conn = new SQLiteConnection("sQLi.db3");
+        conn.open();
+        string query = "select * from keywords";
+
+
+
+    }
+}
